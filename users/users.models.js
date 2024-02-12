0a76907939d4398a8adf8beaf871962f8b4b6f6f
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-02-12 19:56:43 +0100

    feat: user mongodb schema created

---
+++ users/users.models.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const paymentStatuses = ["pending", "paid", "failed", "refunded"];
+
+const UserSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+
+		password: {
+			type: String,
+			required: true,
+		},
+
+		paymentStatus: {
+			type: String,
+			enum: paymentStatuses,
+			default: "pending",
+		},
+		image: {
+			type: String,
+		},
+		address: {
+			type: Object,
+		},
+		phone: {
+			type: String,
+		},
+		isActive: {
+			type: Boolean,
+			default: true,
+		},
+	},
+	{ timestamps: true },
+);
+
+UserSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) {
+		next();
+	}
+	const salt = await bcrypt.genSaltSync(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
+});
+
+export default mongoose.model("User", UserSchema);
