c31dcc44bd16643667709ece70359e6fe6954f1a
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-02-13 22:22:31 +0100

    feat: stripe implementation

---
+++ stripe/stripe.controllers.js
@@ -0,0 +1,98 @@
+import stripe from "stripe";
+import asyncHandler from "express-async-handler";
+import User from "../users/users.models.js";
+
+// handle charge creation
+const createCharge = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+  const { amount, source } = req.body;
+  const stripeClient = stripe(req.stripeApiKey);
+
+  try {
+    const charge = await stripeClient.charges.create({
+      amount,
+      currency: "usd",
+      source,
+      description: "Charge test",
+    });
+
+    // Update user paymentTransactionId
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { paymentTransactionId: charge.id },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      console.log("User not found for userId:", userId);
+    } else {
+      console.log(
+        "User paymentTransactionId updated successfully:",
+        updatedUser
+      );
+    }
+
+    res.status(200).json("Payment successful");
+  } catch (error) {
+    console.error("Error in processing payment", error);
+
+    let msg = "An error occurred while processing your payment";
+
+    if (error.type === "StripeCardError") {
+      msg = error.message;
+    }
+    res.status(500).json("Payment unsuccessful", msg);
+  }
+});
+
+// handle webhook event
+const webhookController = async (req, res) => {
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    console.error("Error processing webhook:", error);
+    res.status(400).json(`Webhook Error: ${error.message}`);
+    return;
+  }
+
+  switch (event.type) {
+    case "charge.succeeded": {
+      const chargeSucceeded = event.data.object;
+      const user = await User.findOneAndUpdate(
+        { paymentTransactionId: chargeSucceeded.id },
+        { paymentStatus: "paid" },
+        { new: true }
+      );
+
+      if (!user) {
+        console.log("No transaction done for this user:");
+      } else {
+        console.log("User payment status updated successfully:");
+      }
+      break;
+    }
+    case "charge.failed": {
+      const chargeFailed = event.data.object;
+
+      console.log("Payment failed for chargeSucceeded:", chargeFailed.id);
+      break;
+    }
+
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).end();
+};
+
+
+
+export { createCharge, webhookController };
---
+++ stripe/stripe.routes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  createCharge,
+  webhookController,
+  getWebhookList,
+} from "./stripe.controllers.js";
+import stripeAuthMiddleware from "../middleware/stripeAuth.js";
+import { authenticateUser } from "../middleware/authenticate.js";
+const stripeRouter = express.Router();
+stripeRouter.use(authenticateUser, stripeAuthMiddleware);
+
+stripeRouter.post(
+  "/charge",
+
+  createCharge
+);
+
+// Route for handling webhook events
+stripeRouter.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+
+  webhookController
+);
+
+export default stripeRouter;
